eea86eb64b30d82f2253f1924cc4561de3586019
JaySzn992000 <121809806+JaySzn992000@users.noreply.github.com>
2025-08-30 07:48:15 +0530

    Update AdminUpdate.js

--- src/AdminUpdate.js
+++ src/AdminUpdate.js
@@ -60,7 +60,7 @@ e.preventDefault();
 if (!validateForm()) return;
 
 try {
-const response = await fetch("https://namasyaa.onrender.com/updateAdminSimple", {
+const response = await fetch("https://lavonne.onrender.com/updateAdminSimple", {
 method: "POST",
 headers: {
 "Content-Type": "application/json",
