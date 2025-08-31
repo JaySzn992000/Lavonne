390df4cb5e49d24c3291ccad6d1501928bfbe9f7
JaySzn992000 <121809806+JaySzn992000@users.noreply.github.com>
2025-08-31 16:44:14 +0530

    Update Registeration.js

--- src/RegisterationsLogin/Registeration.js
+++ src/RegisterationsLogin/Registeration.js
@@ -86,7 +86,7 @@ const isValid = onsubmit();
 if (!isValid) return; 
 
 try {
-const response = await fetch("https://namasyaa.onrender.com/registerationPost", {
+const response = await fetch("https://lavonne-c8o3.onrender.com/registerationPost", {
 method: "POST",
 headers: {
 "Content-Type": "application/json",
