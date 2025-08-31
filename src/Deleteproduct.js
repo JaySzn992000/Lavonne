d8e8385d738fb01e1c1c339247c0b99095e8f270
JaySzn992000 <121809806+JaySzn992000@users.noreply.github.com>
2025-08-31 13:47:32 +0530

    Update Deleteproduct.js

--- src/Deleteproduct.js
+++ src/Deleteproduct.js
@@ -10,7 +10,7 @@ const handleDelete = async (e) => {
 e.preventDefault();
 
 try {
-const response = await fetch("https://namasyaa.onrender.com/deletebyname", {
+const response = await fetch("https://lavonne-c8o3.onrender.com/deletebyname", {
 method: "POST",
 headers: {
 "Content-Type": "application/json",
