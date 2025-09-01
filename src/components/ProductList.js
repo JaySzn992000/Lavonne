c8ef6b4a066f950adc60344523da34860fd7fc3b
JaySzn992000 <121809806+JaySzn992000@users.noreply.github.com>
2025-09-01 07:37:34 +0530

    Update ProductList.js

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -23,7 +23,7 @@ JSON.parse(localStorage.getItem("wishlistStatus")) || {};
 setWishlistStatus(storedWishlistStatus);
 
 axios
-.get("https://naturalbuti-rpvg.onrender.com/fetchProductslist")
+.get("https://lavonne-0729.onrender.com/fetchProductslist")
 .then((response) => {
 setArrayStore(response.data);
 setFilteredProducts(response.data);
@@ -40,7 +40,7 @@ const query = new URLSearchParams(location.search).get("search");
 useEffect(() => {
 if (query) {
 axios
-.get("https://naturalbuti-rpvg.onrender.com/fetchProductslist", {
+.get("https://lavonne-0729.onrender.com/fetchProductslist", {
 params: { search: query },
 })
 .then((response) => {
@@ -52,7 +52,7 @@ console.error("Error fetching products:", error);
 });
 } else {
 axios
-.get("https://naturalbuti-rpvg.onrender.com/fetchProductslist")
+.get("https://lavonne-0729.onrender.com/fetchProductslist")
 .then((response) => {
 setAllProducts(response.data);
 setFilteredProducts(response.data);
@@ -161,7 +161,7 @@ wishlistStatus[productlist.id] ? "wishlist-active" : ""
 <Link to={`/products/${slugify(productlist.name)}/${productlist.id}`}>
 
 <img
-src={`https://naturalbuti-rpvg.onrender.com${productlist.file_path}`}
+src={`https://lavonne-0729.onrender.com${productlist.file_path}`}
 alt={productlist.name}
 loading="lazy"
 />
