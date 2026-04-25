9943f542c60d96fb20319e37e7d7546d38d99361
Jay Kumar <jay.k.992000@gmail.com>
2026-04-25 23:55:34 +0530

    updated slider css

--- src/components/allproducts.js
+++ src/components/allproducts.js
@@ -131,7 +131,7 @@ wishlistStatus[productlist.id] ? "wishlist-active" : ""
 <Link to={`/products/${slugify(productlist.name)}/${productlist.id}`}>
 
 <img
-src={`https://lavonne-0729.onrender.com/${productlist.file_path}`}
+src={productlist.file_path}
 alt={productlist.name}
 loading="lazy"
 />
